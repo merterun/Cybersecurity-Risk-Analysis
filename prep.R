f3cfa0660292e3e92a7e12d84ae44acd00a372df
samwise <44654977+merterun@users.noreply.github.com>
2023-05-14 14:53:26 +0300

    Add files via upload

---
+++ prep.R
@@ -0,0 +1,11 @@
+df1209 <- read_csv("2022-12-09-enriched.csv")
+df0704 <- read_csv("2022-07-04-enriched.csv")
+df0627 <- read_csv("2022-06-27-enriched.csv")
+df0609 <- read_csv("2022-06-09-enriched.csv")
+df0608 <- read_csv("2022-06-08-enriched.csv")
+
+all_df <- rbind(df0608, df0609, df0627, df0704, df1209)
+
+head(all_df)
+dim(all_df)
+str(all_df)
