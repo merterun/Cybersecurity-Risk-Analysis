5df6cc71ce5797f3b2ef779be4743d343a18c856
e.mert <44654977+merterun@users.noreply.github.com>
2023-05-17 20:40:45 +0300

    Add files via upload

--- eda.R
+++ eda.R
@@ -1,20 +1,4 @@
-##Distribution of vulnerabilities by vendor
-library(ggplot2)
-
-# Count the number of vulnerabilities per vendor
-vendor_counts <- table(all_imp$vendor_project)
-
-# Create a bar plot to visualize the distribution of vulnerabilities by vendor
-ggplot(data = data.frame(vendor = names(vendor_counts), count = as.numeric(vendor_counts)),
-       aes(x = reorder(vendor, -count), y = count)) +
-  geom_bar(stat = "identity", fill = "steelblue") +
-  labs(x = "Vendor", y = "Number of Vulnerabilities") +
-  ggtitle("Distribution of Vulnerabilities by Vendor") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
 ## Distribution of vulnerabilities by product
-
 library(plotly)
 
 # Create a data frame with product counts
@@ -63,40 +47,24 @@ ggplot(data = vuln_time_series, aes(x = Date, y = Count)) +
   ylab("Number of vulnerabilities") +
   ggtitle("Time Series Analysis of Vulnerabilities")
 
-##Frequency of different types of vulnerabilities (e.g., SQL injection, command injection)
-
-# Frequency of vulnerability types
-vuln_type_counts <- table(all_imp$cwe)
-vuln_type_counts <- as.data.frame(vuln_type_counts, stringsAsFactors = FALSE)
-vuln_type_counts <- vuln_type_counts[order(vuln_type_counts$Freq, decreasing = TRUE), ]
-colnames(vuln_type_counts) <- c("Vulnerability_Type", "Count")
-
-# Bar plot
-ggplot(data = vuln_type_counts, aes(x = Vulnerability_Type, y = Count)) +
-  geom_bar(stat = "identity", fill = "#496713") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  xlab("Vulnerability Type") +
-  ylab("Count") +
-  ggtitle("Frequency of Vulnerability Types")
-
 
 ##Proportion of vulnerabilities with due dates met
-
 # Calculate the proportion of vulnerabilities with due dates met
 proportion_due_dates_met <- sum(!is.na(all_imp$due_date)) / nrow(all_imp) * 100
 
 # Print the result
 print(paste("Proportion of vulnerabilities with due dates met:", proportion_due_dates_met, "%"))
 
-##Distribution of vulnerabilities by vector (e.g., network, local)
 
+##Distribution of vulnerabilities by vector (e.g., network, local)
 # Count vulnerabilities by vector
 vulnerabilities_by_vector <- all_imp %>%
   group_by(vector) %>%
-  summarise(count = n())
+  summarise(count = n()) %>% 
+  drop_na()
 
 # Print the result
-print(vulnerabilities_by_vector)
+vulnerabilities_by_vector
 
 # Create a pie chart for vulnerabilities by vector
 chart_vector <- ggplot(vulnerabilities_by_vector, aes(x = "", y = count, fill = vector)) +
@@ -107,8 +75,8 @@ chart_vector <- ggplot(vulnerabilities_by_vector, aes(x = "", y = count, fill = 
 
 chart_vector
 
-##Distribution of vulnerabilities by complexity level
 
+##Distribution of vulnerabilities by complexity level
 # Distribution of vulnerabilities by complexity level
 complexity_counts <- table(all_imp$complexity)
 complexity_counts
@@ -116,21 +84,108 @@ complexity_counts
 barplot(complexity_counts, main = "Distribution of Vulnerabilities by Complexity Level", xlab = "Complexity Level", ylab = "Count")
 
 
-##Trend analysis of vulnerability discoveries over time
-
-# Count the number of vulnerabilities discovered per year
-yearly_counts <- all_imp %>%
-  group_by(pub_year) %>%
-  summarise(count = n())
-
-# Plot the trend of vulnerability discoveries over time
-ggplot(yearly_counts, aes(x = pub_year, y = count)) +
-  geom_line() +
-  labs(title = "Trend of Vulnerability Discoveries Over Time",
-       x = "Year", y = "Count")
-
-
 ##Identification of top 10 vendors with the highest number of vulnerabilities
+top_vendors <- all_imp %>%
+  group_by(vendor_project) %>%
+  summarize(total_vulnerabilities = n()) %>%
+  arrange(desc(total_vulnerabilities)) %>%
+  head(10)
+
+top_vendors
+
+# Bar chart for top vendors
+top_vendors_chart <- ggplot(top_vendors, aes(x = reorder(vendor_project, -total_vulnerabilities), y = total_vulnerabilities)) +
+  geom_bar(stat = "identity", fill = "skyblue", width = 0.5) +
+  labs(title = "Top 10 Vendors with the Highest Number of Vulnerabilities",
+       x = "Vendor",
+       y = "Total Vulnerabilities") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+top_vendors_chart
+
 ##Identification of top 10 products with the highest number of vulnerabilities
+top_products <- all_imp %>%
+  group_by(product) %>%
+  summarize(total_vulnerabilities = n()) %>%
+  arrange(desc(total_vulnerabilities)) %>%
+  head(10)
+
+top_products
+
+# Bar chart for top products
+top_products_chart <- ggplot(top_products, aes(x = reorder(product, -total_vulnerabilities), y = total_vulnerabilities)) +
+  geom_bar(stat = "identity", fill = "lightgreen", width = 0.5) +
+  labs(title = "Top 10 Products with the Highest Number of Vulnerabilities",
+       x = "Product",
+       y = "Total Vulnerabilities") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+top_products_chart
+
 ##Analysis of the most common CWE categories among vulnerabilities
+
+common_cwe <- all_imp %>%
+  group_by(cwe) %>%
+  summarize(total_vulnerabilities = n()) %>%
+  arrange(desc(total_vulnerabilities)) %>% 
+  head(10) %>% 
+  drop_na()
+
+common_cwe
+
+# Bar chart for common CWE categories
+common_cwe_chart <- ggplot(common_cwe, aes(x = reorder(cwe, -total_vulnerabilities), y = total_vulnerabilities)) +
+  geom_bar(stat = "identity", fill = "orange", width = 0.5) +
+  labs(title = "Most Common CWE Categories among Vulnerabilities",
+       x = "CWE Category",
+       y = "Total Vulnerabilities") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+common_cwe_chart
+
+
+# Analyze patching speed
+all_imp$patching_time <- all_imp$due_date - all_imp$pub_date
+
+all_imp$patching_time <- as.numeric(all_imp$patching_time)
+
+summary(all_imp$patching_time)
+
+
+hist(all_imp$patching_time, breaks = 20, xlab = "Patching Time (Days)", main = "Distribution of Patching Time")
+
+sum(is.na(all_imp$severity))
+sum(is.na(all_imp$patching_time))
+
+is.finite(all_imp$severity)
+is.finite(all_imp$patching_time)
+
+
+
+# Severity vs Patching Time
+plot(all_num_clean$severity, all_imp$patching_time, xlab = "Severity", ylab = "Patching Time (Days)", main = "Severity vs. Patching Time")
+
+
+cor(all_num_clean$severity, all_imp$patching_time)
+
+
+
+library(zoo)
+
+# Convert date_added column to year-month format
+all_imp$date_added <- as.yearmon(all_imp$date_added)
+
+# Count the number of vulnerabilities by year and month
+vuln_counts <- table(all_imp$date_added)
+
+# Plot the time series of vulnerability counts
+plot(vuln_counts, xlab = "Date", ylab = "Number of Vulnerabilities",
+     main = "Trends in Vulnerability Counts")
+
+# If needed, you can also aggregate the data by severity level
+severity_counts <- table(all_imp$date_added, all_imp$severity)
+
+# Plot the time series of vulnerability counts by severity
+plot(severity_counts, xlab = "Date", ylab = "Number of Vulnerabilities",
+     main = "Trends in Vulnerability Counts by Severity")
 
