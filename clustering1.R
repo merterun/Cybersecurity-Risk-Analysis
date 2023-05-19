336ce36aea153d084cdc34df90fbb41fe022ab24
e.mert <44654977+merterun@users.noreply.github.com>
2023-05-19 14:19:42 +0300

    Add files via upload

--- clustering1.R
+++ clustering1.R
@@ -19,3 +19,33 @@ barplot(cluster_counts)
 
 heatmap(table(cluster_data$vendor_project, cluster_data$product))
 
+
+## Vendor Collaboration: Which vendors have collaborated on multiple projects or products? Are there any patterns or clusters of vendors working together?
+
+# Group the data by vendor and count the number of unique projects or products
+vendor_collaboration <- all_imp %>%
+  group_by(vendor_project) %>%
+  summarise(collaborations = n_distinct(product)) %>%
+  filter(collaborations > 1) %>%
+  arrange(desc(collaborations))
+
+# Plot the vendor collaborations
+ggplot(vendor_collaboration, aes(x = reorder(vendor_project, collaborations), y = collaborations)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(x = "Vendor Project", y = "Number of Collaborations") +
+  ggtitle("Vendors with Multiple Collaborations") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# cluster analysis to identify patterns of vendor collaboration
+# Compute distance matrix
+dist_matrix <- dist(all_num_clean$vendor_project)
+
+# Perform hierarchical clustering
+hc <- hclust(dist_matrix)
+
+# Cut the dendrogram to obtain clusters
+clusters <- cutree(hc, k = 3)  # Adjust the value of k as needed
+
+# Create a heatmap of the distance matrix with cluster assignments
+heatmap(as.matrix(dist_matrix), Rowv = as.dendrogram(hc), Colv = NA, col = heat.colors(256),
+        main = "Clustered Heatmap", labRow = "", labCol = clusters)
